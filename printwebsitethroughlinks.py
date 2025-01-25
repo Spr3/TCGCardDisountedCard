0cf2d7f32083596fccd4dbb21613d0a746aa8fde
Sapire <89864486+Spr3@users.noreply.github.com>
2025-01-25 12:40:46 -0700

    this just gave me the list

---
+++ printwebsitethroughlinks.py
@@ -0,0 +1,2 @@
+for i in range(200):
+    print(f"\"https://www.tcgplayer.com/search/pokemon/product?productLineName=pokemon&page={i+1}&view=grid&ProductTypeName=Cards&Condition=Near+Mint&inStock=true&Rarity=Ultra+Rare|Rare|Promo\",")
